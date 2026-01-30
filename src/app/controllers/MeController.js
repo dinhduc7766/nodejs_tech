720b2466aae80c15c05be51b12c4b25a5c4d7c55
Dang Dinh Duc <ad@ads-MacBook-Pro.local>
2026-01-30 16:31:10 +0700

    feat: update course

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -2,9 +2,13 @@ const Course = require('../models/Course');
 const { multipleMongooseToObject } = require('../../util/mongoose');
 
 class MeController {
-    // [GET] /stored/courses
-    storedCourses(req, res) {
-        res.render('me/stored_courses');
+    // [GET] /me/stored/courses
+    storedCourses(req, res, next) {
+        Course.find({})
+            .then((courses) => res.render('me/stored-courses' , {
+                courses: multipleMongooseToObject(courses)
+            }))
+            .catch(next)
     }
 }
 
